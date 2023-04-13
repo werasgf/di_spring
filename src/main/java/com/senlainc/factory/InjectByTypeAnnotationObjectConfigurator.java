11830fc383db6d7396438e807acd9dff6345359b
werasgf <tasher02@list.ru>
2023-04-13 17:26:03 +0300

    custom_di

---
+++ src/main/java/com/senlainc/factory/InjectByTypeAnnotationObjectConfigurator.java
@@ -0,0 +1,22 @@
+package com.senlainc.factory;
+
+import com.senlainc.config.ApplicationContext;
+import com.senlainc.di.InjectByType;
+import com.senlainc.factory.ObjectConfigurator;
+import lombok.SneakyThrows;
+
+import java.lang.reflect.Field;
+
+public class InjectByTypeAnnotationObjectConfigurator implements ObjectConfigurator {
+    @Override
+    @SneakyThrows
+    public void configure(Object t, ApplicationContext context) {
+        for (Field field : t.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(InjectByType.class)) {
+                field.setAccessible(true);
+                Object object = context.getObject(field.getType());
+                field.set(t, object);
+            }
+        }
+    }
+}
--- src/main/java/com/senlainc/repository/FilmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senlainc.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class FilmRepository {
-}
